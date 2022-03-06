69586c59622870950dc3f8f12fab2f52d25e8b52
Pyodin <paul_bourhis@yahoo.com>
2022-03-06 12:48:42 +0100

    add pyqt5 + main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+# coding: utf-8
+from PyQt5 import QtWidgets, uic
+import sys
+
+
+class MainWindow(QtWidgets.QMainWindow):
+
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+
+        # Load the UI Page
+        uic.loadUi("main.ui", self)
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    main = MainWindow()
+    main.show()
+    sys.exit(app.exec_())
